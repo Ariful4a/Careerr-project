8120094115a5f86163e03466a90b373088c13ea2
Ariful islam <afkgamingno1@gmail.com>
2024-10-29 06:26:24 +0600

    add all code

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+ 
+module.exports = {
+  purge: [],
+  darkMode: "media", // or 'media' or 'class'
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: '1rem',
+      },
+      maxWidth: {
+        'custom': '1170px',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
+
